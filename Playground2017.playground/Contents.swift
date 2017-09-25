c4763fcdd3f80fb494792bde48ac466cf6e89687
Amber Bullough <amber@bertbullough.com>
2017-09-25 08:34:26 -0600

    Added in own variable and removed error

--- Playground2017.playground/Contents.swift
+++ Playground2017.playground/Contents.swift
@@ -4,23 +4,30 @@ import UIKit
 
 //Explicit Type declatation with no value.
 var ramen :String
+
 //Implicit Type declaration with a value to assign type.
 var soup = "Ramen"
 ramen = soup
 print(ramen)
 print(soup)
 //ramen = 7
+
 var myAge :Int
 myAge = -89
 print(myAge)
 myAge = myAge * 3
 var someValue = 3.1415265359 * 56
 someValue = someValue * 231.1
+
 var isReal :Bool
 isReal = false
 isReal = true
+
 let myName :String
-myname = "Amber"
+myName = "Amber"
 print(myName)
 //myName = soup
 //Can't assign into a let once assigned.
+
+var heart = "Love"
+print(heart)
