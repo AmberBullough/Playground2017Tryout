41de0a5e43a123b60814628286e95e4becbbf3b1
Amber Bullough <amber@bertbullough.com>
2017-09-25 09:04:03 -0600

    Strings edit

--- String.playground/Contents.swift
+++ String.playground/Contents.swift
@@ -4,9 +4,27 @@ import UIKit
 
 var str = "Hello, playground"
 
+var Blah = "Blah Blah Blah"
+Blah = "He He He"
+print (Blah)
+
+let multiplier = 3
+let message = " 3 times 2.5 is 7.5"
+//message is "is "3 * 2.5 = 7.5"
+print (message)
+
 let unusualMenagerie = "Unicorn, Clover, Ladybug"
 print("unusualMenagerie has 24 characters")
 //prints "unusualMenagerie has 40 characters"
 
 var word = "cafe"
 print("the number of characters in cafe is 4")
+
+
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+    print(quotation + sameQuotation)
+}
+// Prints "These two strings are considered equal"
