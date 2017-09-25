aa92aacc6b5e9a55a05fa85c49f9866dea7897f1
Amber Bullough <amber@bertbullough.com>
2017-09-25 09:22:38 -0600

    FInished myViews and need to start button

---
+++ UI.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
+let myView = UIView (frame: rect)
+
+let rect2 = CGRect(x: 50, y:100, width: 500, height: 1000)
+let theView = UIView (frame: rect2)
+
+let rect3 = CGRect(x: 2, y:4, width: 2, height: 4)
+let whatView = UIView (frame: rect3)
+
