a1c94f971160f8ee7eac9c5efe93e55411225ec8
Amber Bullough <amber@bertbullough.com>
2017-09-27 09:11:16 -0600

    Added in if, else, while statement

--- MethodsPractice.playground/Contents.swift
+++ MethodsPractice.playground/Contents.swift
@@ -18,3 +18,35 @@ public func evenLessSimpleMethod(count :Int, bandName :String, date :String) -> 
     print(info)
 }
 evenLessSimpleMethod(count: 4, bandName: "They Might Be Giants", date: "June, 1992")
+
+
+
+
+public func calculateDays(first firstDay :Int, secondDay :Int) -> Int
+{
+    let answer = secondDay - firstDay
+    
+    return answer
+}
+calculateDays(first: 4, secondDay:5)
+public func name(friendName homeName :String) -> Void
+{
+    let info = "At home my name is " + homeName
+    print(info)
+}
+name (friendName: "Amber.")
+
+var guess = 453
+if (guess < 500)
+{
+    print("Wahoo!")
+}
+else if(guess == 65)
+{
+    print("Not likely")
+}
+while (guess > 0)
+{
+    print("Hahaha \(guess)")
+    guess -= 1
+}
